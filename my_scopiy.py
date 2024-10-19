a4b67a90b7d601c43c70d5c0b1eacb609eef2529
Oleh <314koval.ok@gmail.com>
2024-10-19 14:37:29 +0300

    realis_v2

--- my_scopiy.py
+++ my_scopiy.py
@@ -1,4 +1,18 @@
 def find_intervents(row, mn, name):
+    """
+    Класифікує рядок
+
+    Ця функцiя приймае строку та повертае клас до якоi вона вiноситься.
+    Крiм строки ще подаеться список значень для класифiкацii та назви
+    класу
+
+    Аргументи:
+    row (str): Строка для класифiкацii
+    mn (str, list, set): Список з множествами
+    якi служать для визначення до якоi групи вiднести запис
+    name (str, list): Список назв класiв
+    
+    """
     row_set = set(row.split(","))
     for index, value in enumerate(range(len(mn))):
         element = len(mn[index] & row_set)
@@ -9,7 +23,7 @@ def find_intervents(row, mn, name):
 if __name__ == "__main__":
     
     mn = [{'a', 'b'}, {'c', 'd'}, {'e', 'f'}]
-    mnn = "f,c,f,j,k,m,n,c,e"
+    mnn = "f,c,a,f,j,k,m,n,c,e"
     name = ['Колоноскопия', 'Эндоскопия', 'Гистероскопия']
 
     print(find_intervents(mnn, mn, name))
